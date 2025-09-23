63b9507c432a4e0956dbb80899abaf44eae42491
DreamerVulpi <dreamervulpi@gmail.com>
2025-09-23 21:02:34 +0300

    [fix] init_tables

--- migrations/00001_initial_tables.sql
+++ migrations/00001_initial_tables.sql
@@ -36,8 +36,8 @@ CREATE TABLE sets
     id SERIAL PRIMARY KEY,
     player1_id INTEGER REFERENCES users (id),
     player2_id INTEGER REFERENCES users (id),
-    pool_id INTEGER
-    -- pool_id INTEGER REFERENCES pools (id)
+    -- pool_id INTEGER
+    pool_id INTEGER REFERENCES pools (id)
 );
 -- +goose StatementEnd
 
